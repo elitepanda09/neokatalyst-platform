4c9a453ddf2b5d16527b045b532dbff6c757ff8f
elitepanda09 <143698249+elitepanda09@users.noreply.github.com>
2025-07-01 14:38:11 +0200

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -25,6 +25,9 @@ import os
 ROOT_DIR = Path(__file__).resolve().parent.parent
 load_dotenv(ROOT_DIR / ".env")
 
+MONGO_URL = 'mongodb+srv://neokatalyst-admin:GvqlxkwXkiFM3PGB@cluster0neokatalyst-clu.eq0eg1j.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0neokatalyst-cluster'
+DB_NAME = 'neokatalyst_production'
+
 print("✅ MONGO_URL:", os.environ.get("MONGO_URL"))
 print("✅ DB_NAME:", os.environ.get("DB_NAME"))
 
